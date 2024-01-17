1f8db0911f6f8063ac462493b4da095fa9359a07
Jules-gatete <j.gatete@alustudent.com>
2024-01-17 00:28:57 -0800

    console,FileStorage and tests updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,25 +1,37 @@
-#!/usr/bin/python3
+'''
+FileStorage class
+'''
 
 import json
 
+FILE_PATH = 'file.json'
+
+
 class FileStorage:
-    __file_path = "file.json"
+    '''
+    FileStorage class
+
+    Attributes:
+        __file_path: string - path to the JSON file
+        __objects: dictionary - empty but will
+        store all objects by <class name>.id
+    '''
+    __file_path = FILE_PATH
     __objects = {}
 
-    @property
-    def file_path(self):
-        return self.__file_path
-
-    @property
-    def _FileStorage__objects(self):
-        return FileStorage.__objects
-
     def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+        '''
+        Returns the dictionary __objects
+        '''
+        return FileStorage.__objects.copy()
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        '''
+        Sets in __objects the obj with key <obj class name>.id
+
+        Args:
+            obj: object to set in __objects
+        '''
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
@@ -31,15 +43,15 @@ class FileStorage:
             self.save()
 
     def save(self):
-        """ Serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, mode='w', encoding="utf-8") as f:
-            copied_dict = {}
+    
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as f:
+            casted_dict = {}
             for key, value in FileStorage.__objects.items():
-                copied_dict[key] = value.to_dict()
-            json.dump(copied_dict, f)
-    def reload(self):
-        """Deserialize the JSON file to __objects"""
+                casted_dict[key] = value.to_dict()
+            json.dump(casted_dict, f)
 
+    def reload(self):
+        
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
@@ -47,11 +59,12 @@ class FileStorage:
         from models.place import Place
         from models.amenity import Amenity
         from models.review import Review
+
         try:
-            with open(self.file_path, mode='r', encoding="utf-8") as f:
+            with open(FileStorage.__file_path, "r", encoding='utf-8') as f:
                 data = json.load(f)
                 for key, value in data.items():
-                     FileStorage.__objects[key] = eval(
+                    FileStorage.__objects[key] = eval(
                         value["__class__"])(**value)
         except NameError:
             pass
